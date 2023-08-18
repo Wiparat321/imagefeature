a0e4ce297c0d5b79f89012a9f14a098df10dd332
64011212158@msu.ac.th <64011212158@msu.ac.th>
2023-08-18 17:44:18 +0700

    first commit

--- app/hog.py
+++ app/hog.py
@@ -11,5 +11,5 @@ def gethog(img_gray):
     block_stride = (8, 8)
     num_bins = 9
     hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, num_bins)
-    hog_descriptor = hog.compute(img_gray)
+    hog_descriptor = hog.compute(new_img)
     return hog_descriptor
